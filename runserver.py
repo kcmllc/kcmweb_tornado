914faa6347b16eff2228fd0464a8e615822cf068
Kyle Gay <kyle.gay@knightcapllc.com>
2014-11-11 00:46:21 -0600

    added evernote page

--- runserver.py
+++ runserver.py
@@ -1,5 +1,6 @@
 import json
 import threading
+import sys
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
@@ -9,10 +10,10 @@ from evernote.api.client import EvernoteClient
 import redis
 import logging
 import tornadoredis
+import thread
 from utils.utils import OpenChannel
 from functools import partial
 import toredis
-
 
 
 class NewMsgHandler(tornado.web.RequestHandler):
@@ -25,9 +26,7 @@ class NewMsgHandler(tornado.web.RequestHandler):
         r.publish('realtime', data)
 
 
-
 class RealtimeHandler(SockJSConnection):
-
     clients = set()
 
     def on_open(self, info):
@@ -50,12 +49,24 @@ class RealtimeHandler(SockJSConnection):
                 c.send(msg.body)
 
 
-class EvernoteHandler(tornado.websocket.WebSocketHandler):
-    def __init__(self):
-        self.devtoken = self.settings['EVERNOTE_TOKEN']
-        self.client = EvernoteClient(token=self.devtoken)
-        self.user_store = self.client.get_user_store()
-        self.user = self.user_store.getUser()
+class EvernoteHandler(tornado.web.RequestHandler):
+    # def prepare(self):
+
+    # print self.application.settings
+    # self.devtoken = self.application.settings['EVERNOTE_TOKEN']
+    # self.client = EvernoteClient(token=self.devtoken)
+    # self.user_store = self.client.get_user_store()
+    # self.user = self.user_store.getUser()
+    #
+    def get(self, *args, **kwargs):
+        print self.application.settings
+        self.render('evernote.html')
+
+    def post(self, *args, **kwargs):
+        title = self.get_argument('title')
+        text = self.get_argument('text')
+        r = redis.Redis()
+        r.publish('realtime', 'New Evernote message saved')
 
 
 class TornadoApplication(tornado.web.Application):
@@ -66,25 +77,32 @@ class TornadoApplication(tornado.web.Application):
         ]
         self.handlers += handlers
         self.settings = {
-            'EVERNOTE_TOKEN':'S=s1:U=8ec46:E=150eea5c34e:C=14996f49620:P=1cd:A=en-devtoken:V=2:H=4e69f82d21ec4b94c2971c9a5893ad2b'
+            'EVERNOTE_TOKEN': 'S=s1:U=8ec46:E=150eea5c34e:C=14996f49620:P=1cd:A=en-devtoken:V=2:H=4e69f82d21ec4b94c2971c9a5893ad2b'
         }
+
         self.data = set()
         self.sockets = []
         # self.redis = redis.Redis().pubsub().subscribe('realtime')
         self.redis_channels = {}
-        tornado.web.Application.__init__(self, self.handlers, self.settings)
+        super(TornadoApplication, self).__init__(self.handlers, **self.settings)
 
 
 if __name__ == '__main__':
-    port=5000
+    port = 5000
     key = 'realtime'
     import logging
+
     logging.getLogger().setLevel(logging.DEBUG)
-    rclient = tornadoredis.Client(host='localhost', port=6379).connect()
-
+    rclient = tornadoredis.Client(host='localhost', port=6379)
+    rclient.connect()
     Router = SockJSRouter(RealtimeHandler, '/realtime')
-
-    threading.Thread(rclient.psubscribe('realtime', lambda s: rclient.listen(RealtimeHandler.pubsub))).start()
+    redis_thread = threading.Thread(rclient.psubscribe('realtime', lambda s: rclient.listen(RealtimeHandler.pubsub)))
+    try:
+        # redis_thread.setDaemon(True)
+        redis_thread.start()
+    except(KeyboardInterrupt, SystemExit):
+        redis_thread.__Thread__exit()
+        sys.exit()
     app = TornadoApplication(Router.urls)
     app.listen(5000)
     # http_server = tornado.httpserver.HTTPServer(Application())
